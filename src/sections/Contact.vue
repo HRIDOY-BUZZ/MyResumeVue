b27cbbc6614d1f240593127ea0aad95c05be76d6
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2023-04-09 06:39:54 +0600

    Contact Form added back

--- src/sections/Contact.vue
+++ src/sections/Contact.vue
@@ -6,9 +6,41 @@
                     Contact Me
                 </h1>
                 <div class="row pt-5">
-                    <div class="col-sm-12 hb-footer-address">
-                        <div class="row">
-                            <div class="col-sm-12 col-md-4">
+                    <!-- 
+                    ====================
+                        Contact Form
+                    ====================
+                    -->
+                    <div class="col-xl-6 col-lg-5 col-md-12 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">
+                        <form id="contactForm" class="single-form quate-form wow fadeInUp" method="POST">
+                            <div id="msgSubmit" class="h3 text-center hidden"></div>
+                            <div class="row">
+                                <div class="col-md-6 col-sm-12">
+                                    <input name="fname" class="contact-name form-control" id="name" type="text" pattern="([a-zA-Z]([\w -]*[a-zA-Z])?){3,30}" placeholder="First Name" required>
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+                                    <input name="lname" class="contact-email form-control" id="L_name" type="text" pattern="([a-zA-Z]([\w -]*[a-zA-Z])?){3,30}" placeholder="Last Name" required>
+                                </div>
+                                <div class="col-sm-12">
+                                    <input name="email" class="contact-subject form-control" id="email" type="email" placeholder="Your Email Address" required>
+                                </div>
+                                <div class="col-sm-12">
+                                    <textarea name="message" class="contact-message" id="message" rows="6" placeholder="Write Something to Me!" required></textarea>
+                                </div>
+                                <div class="btn-form col-sm-12">
+                                    <button type="submit" class="btn btn-fill btn-block" name="submit" id="form-submit">Send Message</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- 
+                    ====================
+                        Contact Cards
+                    ====================
+                    -->
+                    <div class="col-xl-6 col-lg-7 col-md-12 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s">
+                        <div class="row hb-footer-address mt-5">
+                            <div class="col-md-12 col-lg-6">
                                 <div class="hb-address-footer-item dark-bg shadow-1 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">
                                     <div class="each-icon">
                                         <i class="fa fa-location-arrow"></i>
@@ -21,7 +53,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-sm-12 col-md-4">
+                            <div class="col-md-12 col-lg-6">
                                 <div class="hb-address-footer-item dark-bg shadow-1 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s">
                                     <div class="each-icon">
                                         <i class="fa fa-envelope"></i>
@@ -33,50 +65,8 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-sm-12 col-md-4">
-                                <div class="hb-address-footer-item dark-bg shadow-1 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s">
-                                    <div class="each-icon">
-                                        <i class="fa fa-phone"></i>
-                                    </div>
-                                    <div class="each-info">
-                                        <h4>Phone</h4>
-                                        <a href="callto:+8801907075935">+880 1521 326125</a><br>
-                                        <a href="callto:+8801677472329">+880 1677 472329</a>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
-                        <!-- 
-                        ====================
-                            Contact Form
-                        ====================
-                         -->
-                        <!-- <div class="col-sm-12 col-md-6 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">
-                            <form id="contactForm" class="single-form quate-form wow fadeInUp" method="POST">
-                                <div id="msgSubmit" class="h3 text-center hidden"></div>
-                                <div class="row">
-                                    <div class="col-md-6 col-sm-12">
-                                        <input name="fname" class="contact-name form-control" id="name" type="text" pattern="([a-zA-Z]([\w -]*[a-zA-Z])?){3,30}" placeholder="First Name" required>
-                                    </div>
-                                    <div class="col-md-6 col-sm-12">
-                                        <input name="lname" class="contact-email form-control" id="L_name" type="text" pattern="([a-zA-Z]([\w -]*[a-zA-Z])?){3,30}" placeholder="Last Name" required>
-                                    </div>
-                                    <div class="col-sm-12">
-                                        <input name="email" class="contact-subject form-control" id="email" type="email" placeholder="Your Email" required>
-                                    </div>
-                                    <div class="col-sm-12">
-                                        <textarea name="message" class="contact-message" id="message" rows="6" placeholder="Your Message" required></textarea>
-                                    </div>
-                                    <div class="btn-form col-sm-12">
-                                        <button type="submit" class="btn btn-fill btn-block" name="submit" id="form-submit">Send Message</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="col-sm-12 col-md-6 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s">
-                            
-                        </div> -->
                 </div>
             </div>
         </div>
