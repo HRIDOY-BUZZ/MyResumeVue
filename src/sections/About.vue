72a0fa6766d8d33d83e20d0f6daf59f2268b9532
Al-Amin Islam Hridoy <36563890+HRIDOY-BUZZ@users.noreply.github.com>
2024-05-19 08:08:42 +0600

    minor updates

--- src/sections/About.vue
+++ src/sections/About.vue
@@ -3,36 +3,38 @@
 </script>
 <template>
     <main class="hb-about pt-5" id="hb-about">
-            <div class="container">
-                <div class="row section-separator">
-                    <div class="col-sm-12 col-md-6">
-                        <div class="hb-about-img shadow-2 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
-                            <img src="/assets/images/ab-img.webp" alt="Al-Amin Islam Hridoy" class="img-fluid" loading="lazy">
-                        </div>
+        <div class="container">
+            <div class="row section-separator">
+                <div class="col-sm-12 col-md-6">
+                    <div class="hb-about-img shadow-2 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
+                        <img src="/assets/images/ab-img.webp" alt="Al-Amin Islam Hridoy" class="img-fluid" loading="lazy">
                     </div>
-                    <div class="col-sm-12 col-md-6">
-                        <div class="hb-about-inner">
-                            <h2 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.1s">About Me</h2>
-                            <p class="wow fadeInUp text-justified" data-wow-duration="0.8s" data-wow-delay="0.2s">
-                                I am a full-stack Web Developer (Laravel/PHP), having 2+ years of hands-on
-                                experience in both front-end designing and back-end development. Familiar with all
-                                stages of the development cycle for dynamic web projects. Currently looking for a
-                                place where I can learn more and grow.
-                            </p>
-                            <div class="hb-about-tag wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">
-                                <ul>
-                                    <li v-for="skill in skills" :key="skill">
-                                        <span>{{ skill.skill }}</span>
-                                    </li>
-                                </ul>
-                            </div>
-                            <a href="/assets/files/CV_Al-Amin_Islam_Hridoy.pdf" class="btn btn-fill wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">Downlaod CV <i class="fa fa-download"></i></a>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <div class="hb-about-inner">
+                        <h2 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.1s">About Me</h2>
+                        <p class="wow fadeInUp text-justified" data-wow-duration="0.8s" data-wow-delay="0.2s">
+                            I am a full-stack Web Developer (Laravel/PHP), having 2+ years of hands-on
+                            experience in both front-end designing and back-end development. Familiar with all
+                            stages of the development cycle for dynamic web projects. Currently looking for a
+                            place where I can learn more and grow.
+                        </p>
+                        <div class="hb-about-tag wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">
+                            <ul>
+                                <li v-for="skill in skills" :key="skill">
+                                    <span>{{ skill.skill }}</span>
+                                </li>
+                            </ul>
                         </div>
+                        <a href="/assets/files/CV_Al-Amin_Islam_Hridoy.pdf" class="btn btn-fill wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
+                            Downlaod CV <i class="fa fa-download"></i>
+                        </a>
                     </div>
                 </div>
             </div>
-        </main>
-        <Services />
+        </div>
+    </main>
+    <Services />
 </template>
 
 <script>
--- src/sections/Home.vue
+++ src/sections/Home.vue
@@ -9,20 +9,55 @@
                                 <span>Hello I'm</span>
                             </div>
                             
-                            <h2 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s">Al-Amin Islam Hridoy</h2>
-                            <h4 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">Full-stack Web Developer</h4>
+                            <h2 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s">
+                                Al-Amin Islam Hridoy
+                            </h2>
+                            <h4 class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">
+                                Full-stack Web Developer
+                            </h4>
                             
                             <ul>
-                                <li class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s"><i class="fa fa-envelope"></i><a href="mailto:hridoy.ewu16@gmail.com">hridoy.ewu16@gmail.com</a></li>
-                                <li class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s"><i class="fa fa-phone"></i><a href="callto:+8801521326125">+880 1521 326125</a></li>
-                                <li class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s"><i class="fa fa-map-marker"></i><address>Madartek, Bashabo, Dhaka, Bangladesh</address></li>
+                                <li class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
+                                    <i class="fa fa-envelope"></i>
+                                    <a href="mailto:hridoy.ewu16@gmail.com">
+                                        hridoy.ewu16@gmail.com
+                                    </a>
+                                </li>
+                                <li class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s">
+                                    <i class="fa fa-phone"></i>
+                                    <a href="callto:+8801521326125">
+                                        +880 1521 326125
+                                    </a>
+                                </li>
+                                <li class="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s">
+                                    <i class="fa fa-map-marker"></i>
+                                    <address>
+                                        Madartek, Bashabo, Dhaka, Bangladesh
+                                    </address>
+                                </li>
                             </ul>
                             
                             <ul class="social-icon wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.7s">
-                                <li><a href="https://www.facebook.com/hridoy.al.amin.6/"><i class="fab fa-facebook"></i></a></li>
-                                <li><a href="https://twitter.com/hridoy_alamin"><i class="fab fa-twitter"></i></a></li>
-                                <li><a href="https://github.com/HRIDOY-BUZZ"><i class="fab fa-github"></i></a></li>
-                                <li><a href="https://www.linkedin.com/in/al-amin-islam-hridoy/"><i class="fab fa-linkedin"></i></a></li>
+                                <li>
+                                    <a href="https://www.facebook.com/hridoy.al.amin.6/">
+                                        <i class="fab fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com/hridoy_alamin">
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://github.com/HRIDOY-BUZZ">
+                                        <i class="fab fa-github"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/in/al-amin-islam-hridoy/">
+                                        <i class="fab fa-linkedin"></i>
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                     </div>
