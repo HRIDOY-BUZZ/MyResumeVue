5c2ee17096bbff8e631fe2c15df587130d58c89f
Al-Amin Islam Hridoy <36563890+HRIDOY-BUZZ@users.noreply.github.com>
2026-01-05 12:04:04 +0000

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 import { useRoute, RouterLink } from 'vue-router';
 
 const isMenuOpen = ref(false);
 const route = useRoute();
+const isScrolled = ref(false);
 
 const toggleMenu = () => {
     isMenuOpen.value = !isMenuOpen.value;
@@ -12,10 +13,22 @@ const toggleMenu = () => {
 const isCurrentPage = (path) => {
     return route.path === path;
 };
+
+const handleScroll = () => {
+    isScrolled.value = window.scrollY >= 50;
+};
+
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('scroll', handleScroll);
+});
 </script>
 
 <template>
-    <header class="black-bg hb-header hb-fixed-nav nav-scroll hb-xs-mobile-nav wow fadeInUp" id="hb-header">
+    <header class="black-bg hb-header hb-fixed-nav nav-scroll hb-xs-mobile-nav wow fadeInUp" :class="{ 'nav-strict': isScrolled }" id="hb-header">
         <div class="overlay" @click="toggleMenu" :class="{ active: isMenuOpen }"></div>
         <div class="container">
             <div class="row">
@@ -66,10 +79,29 @@ li.nav-item.active a{
     color: #0bceaf;
 }
 
+.nav-strict {
+    z-index: 99;
+    -webkit-box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+    -moz-box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+    background-color: #202026;
+}
+
+.hb-header.nav-strict {
+    padding: 15px 0;
+    -moz-transition: 0.3s;
+    -o-transition: 0.3s;
+    -webkit-transition: 0.3s;
+    transition: 0.3s;
+}
+
 @media only screen and (max-width: 991px) {
     li.nav-item.active a{
-    color: black;
-}
+        color: black;
+    }
+    .nav-strict .navbar-toggler {
+        margin-top: 0px;
+    }
 }
 
 </style>
